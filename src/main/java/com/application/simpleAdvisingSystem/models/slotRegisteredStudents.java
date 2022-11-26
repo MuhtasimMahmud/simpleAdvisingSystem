28e1a32576f9b2af48abd9693ea2463d43154865
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-11-27 04:13:29 +0600

    updated

---
+++ src/main/java/com/application/simpleAdvisingSystem/models/slotRegisteredStudents.java
@@ -0,0 +1,69 @@
+package com.application.simpleAdvisingSystem.models;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class slotRegisteredStudents {
+
+    @Id
+    String SID;
+    String fName;
+    String lName;
+    String email;
+    String slot;
+
+    public slotRegisteredStudents(){
+
+    }
+
+    public slotRegisteredStudents(String SID, String fName, String lName, String email, String slot) {
+        this.SID = SID;
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.slot = slot;
+    }
+
+
+    public String getSID() {
+        return SID;
+    }
+
+    public void setSID(String SID) {
+        this.SID = SID;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+}
