3bea2db9e2da68282ba16c894997d75afa1c9662
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-11-27 13:23:00 +0600

    updated

--- src/main/java/com/application/simpleAdvisingSystem/controller/studentController.java
+++ src/main/java/com/application/simpleAdvisingSystem/controller/studentController.java
@@ -1,23 +1,40 @@
 package com.application.simpleAdvisingSystem.controller;
 
+import com.application.simpleAdvisingSystem.helper.Message;
+import com.application.simpleAdvisingSystem.models.slotRegisteredStudents;
+import com.application.simpleAdvisingSystem.repositories.slotRegisteredStudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class studentController {
 
+
+    @Autowired
+    slotRegisteredStudentRepository studentRepository;
 
     @GetMapping("/student")
     public String normalUser(){
         return "student/student";
     }
 
-    @RequestMapping("/book_slot")
-    public String bookSlot(){
-        return "redirect: student/student";
+    @RequestMapping(value = "/book_slot", method  = RequestMethod.POST)
+    public String bookSlot(@ModelAttribute("slotRegisteredStudent") slotRegisteredStudents student, Model model, HttpSession session){
+
+        try{
+            studentRepository.save(student);
+            session.setAttribute("message", new Message("Successfully Registered your slot!", "alert-success"));
+
+        }catch (Exception e){
+            e.printStackTrace();
+            session.setAttribute("message", new Message("something went wrong", "alert-danger"));
+        }
+
+        return "redirect:/student";
     }
 
 
--- src/main/java/com/application/simpleAdvisingSystem/repositories/slotRegisteredStudentRepository.java
+++ src/main/java/com/application/simpleAdvisingSystem/repositories/slotRegisteredStudentRepository.java
@@ -1,8 +1,9 @@
 package com.application.simpleAdvisingSystem.repositories;
 
+import com.application.simpleAdvisingSystem.models.slotRegisteredStudents;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface slotRegisteredStudentRepository extends JpaRepository {
+public interface slotRegisteredStudentRepository extends JpaRepository<slotRegisteredStudents, String> {
 
 
 
