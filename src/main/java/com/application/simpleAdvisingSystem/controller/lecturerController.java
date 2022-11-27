41ab7fc5d87999947c80c10da03d9a09b121a25e
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-11-27 13:23:41 +0600

    updated

--- src/main/java/com/application/simpleAdvisingSystem/controller/lecturerController.java
+++ src/main/java/com/application/simpleAdvisingSystem/controller/lecturerController.java
@@ -2,7 +2,11 @@ package com.application.simpleAdvisingSystem.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class lecturerController {
@@ -12,5 +16,15 @@ public class lecturerController {
     public String adminUser(){
         return "lecturer/lecturer";
     }
+
+
+//    @RequestMapping("/admin")
+//    public String adminView(Model model){
+//
+//        List<User> user = patientRepository.findAll();
+//        model.addAttribute("patients", patients);
+//        return "admin/admin";
+//    }
+
 
 }
