8e2960b3d4d0bfaf52d093227a2d2ea9b12418f8
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2022-11-28 19:41:09 +0600

    updated

--- src/main/java/com/application/simpleAdvisingSystem/controller/studentController.java
+++ src/main/java/com/application/simpleAdvisingSystem/controller/studentController.java
@@ -1,14 +1,17 @@
 package com.application.simpleAdvisingSystem.controller;
 
 import com.application.simpleAdvisingSystem.helper.Message;
+import com.application.simpleAdvisingSystem.models.slot;
 import com.application.simpleAdvisingSystem.models.slotRegisteredStudents;
 import com.application.simpleAdvisingSystem.repositories.slotRegisteredStudentRepository;
+import com.application.simpleAdvisingSystem.repositories.slotRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
 public class studentController {
@@ -17,9 +20,16 @@ public class studentController {
     @Autowired
     slotRegisteredStudentRepository studentRepository;
 
+    @Autowired
+    slotRepository slotRepository;
+
     @GetMapping("/student")
-    public String normalUser(){
+    public String normalUser(Model model){
+
+        List<slot> slots = slotRepository.findAll();
+        model.addAttribute("slots", slots);
         return "student/student";
+
     }
 
     @RequestMapping(value = "/book_slot", method  = RequestMethod.POST)
